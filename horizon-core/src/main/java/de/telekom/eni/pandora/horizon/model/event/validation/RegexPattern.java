37673cfa392375b988b309dd013798b703fdb556
Mike Herwig <Mike.Herwig@telekom.de>
2024-12-03 11:45:55 +0100

    feat: minor adjustment of uuid validation regex

--- horizon-core/src/main/java/de/telekom/eni/pandora/horizon/model/event/validation/RegexPattern.java
+++ horizon-core/src/main/java/de/telekom/eni/pandora/horizon/model/event/validation/RegexPattern.java
@@ -7,5 +7,5 @@ package de.telekom.eni.pandora.horizon.model.event.validation;
 public class RegexPattern {
     public static final String ISO8601_TIME = "^(?:[1-9]\\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d(?:\\.\\d{1,9})?(?:Z|[+-][01]\\d:[0-5]\\d)$";
     public static final String EVENT_TYPE = "^[a-zA-Z0-9\\.\\-]*$";
-    public static final String UUID = "\b[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-8][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}\b";
+    public static final String UUID = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-8][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$";
 }
