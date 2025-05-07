1385bfcae83c8229e8ee95b7148ea74acf352cf3
Heiner Egbers <heiner.egbers@telekom.de>
2025-05-07 14:48:06 +0200

    fix: subscription query by event_type

--- horizon-core/src/main/java/de/telekom/eni/pandora/horizon/mongo/repository/SubscriptionsMongoRepo.java
+++ horizon-core/src/main/java/de/telekom/eni/pandora/horizon/mongo/repository/SubscriptionsMongoRepo.java
@@ -4,21 +4,16 @@
 
 package de.telekom.eni.pandora.horizon.mongo.repository;
 
-import de.telekom.eni.pandora.horizon.model.event.DeliveryType;
-import de.telekom.eni.pandora.horizon.model.event.Status;
-import de.telekom.eni.pandora.horizon.mongo.model.MessageStateMongoDocument;
 import de.telekom.eni.pandora.horizon.mongo.model.SubscriptionMongoDocument;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.util.Date;
 import java.util.List;
 
 
 public interface SubscriptionsMongoRepo extends MongoRepository<SubscriptionMongoDocument, String> {
-    //List<SubscriptionMongoDocument> findByEventType(String eventType);
+
+    @Query(value = "{ \"spec.subscription.type\": ?0}")
     List<SubscriptionMongoDocument> findByType(String type);
 }
 
